6f07aea23add552b4b1a5ce663189bafa5f7ea48
cawdev <cameronweimer@gmail.com>
2018-02-28 20:39:42 -0500

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,87 @@
+/* global jQuery */
+(function($) {
+	'use strict';
+
+	var $document = $(document),
+		stickyNavFunc = function() {
+			$(window).scroll(function() {
+				var distanceFromTop = $(document).scrollTop(),
+					$navbar = $('.navbar');
+				
+				distanceFromTop >= $('#up').height() ? 
+				$navbar.fadeIn(400)
+					   .addClass('animated slideInDown fixed') :
+				$navbar.removeClass('slideInDown')
+					   .addClass('fadeOutUp')
+					   .fadeOut(300)
+					   .removeClass('fixed');
+			});
+		},
+		smoothScroll = function() {
+			var scroll = new SmoothScroll('a[href*="#"]', {
+				speed: 1800,
+				easing: 'easeInOutCubic'
+			});
+		},
+		slickFunc = function () {
+			var $multipleItems = $('.multiple-items'),
+				slickOptions = {
+					infinite: true,
+					slidesToShow: 2,
+					dots: true,
+					arrows: false,
+					mobileFirst: true,
+					focusOnSelect: true,
+					variableWidth: true,
+					autoplay: true,
+					autoplaySpeed: 5000,
+					cssEase: 'cubic-bezier(0.86, 0, 0.07, 1)',
+					useTransform: true,
+					speed: 1000,
+					lazyLoad: 'ondemand'
+				}
+			$multipleItems.slick(slickOptions);
+		},
+		progressBarFunc = function () {
+			var winHeight = $(window).height(), 
+				docHeight = $(document).height(),
+				progressBar = $('progress'),
+				max, value;
+
+			max = docHeight - winHeight;
+			progressBar.attr('max', max);
+
+			$(document).on('scroll', function(){
+				value = $(window).scrollTop();
+				progressBar.attr('value', value);
+			});
+		},
+		isScrolledIntoView = function(elem) {
+			var docViewTop = $(window).scrollTop(),
+				docViewBottom = docViewTop + $(window).height(),
+				elemTop = $(elem).offset().top,
+				elemBottom = elemTop + $(elem).height();
+
+			return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+		},
+		fadeInFunc = function () {
+
+			$(window).scroll(function() {
+				$('.scrolled .animated').each(function() {
+				if (isScrolledIntoView(this) === true) {
+					$(this).addClass('fadeInUp');
+				}
+				});
+			});
+		};
+
+	$document.on({
+		ready: function() {
+			stickyNavFunc();
+			smoothScroll();
+			slickFunc();
+			progressBarFunc();
+			fadeInFunc();
+		}
+	});
+})(jQuery);
